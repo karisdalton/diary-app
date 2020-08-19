03e83720e759870281121dd61c0b6a5f7522e8bd
Dalton <daltonkaris2000@gmail.com>
2020-08-19 14:19:01 +0300

    updated archive.html
    Removed static bootstrap and fontawesome

--- JS/script.js
+++ JS/script.js
@@ -1,59 +1,99 @@
-var date = document.getElementById ('#date');
-var name = document.getElementById ('#name');
-var description = document.getElementById ('#summernote');
-var addButton = document.getElementById ("#add");
-var saveEntry = document.getElementById("#isOutput");
-const menu = document.querySelector('.sidebar');
-this.entries = [];
-
-function Diary (date, name, description){
-    const storageEntries = localStorage.getItem("entries"); //loads entries from local storage instead of creating an empty array.
-    this.entries = storageEntries  ? JSON.parse (storageEntries):[]; //store our entries from local storage to storageEntries variable.
-    this.dateInput = date;
-    this.nameInput= name;
-    this.descriptionInput = description;
-    this.addButton = addButton;
-    this.saveEntry = document.getElementById ('#isOutput');
-    this.setEvents();
-}
-function setEvents(){
-    this.addButton.onclick = (event) => {
-            event.preventDefault();
-            var entry = newEntry(date.value, name.value, description.value);
-            this.entries.push(entry);  
-            localStorage.setItem ("entries", JSON.stringify(this.entries)); //store new entries in entries array and local storage since working only with the latter would require constant conversion of object entries to strings and back. 
-            this.saveEntry();   
-         };
+class Event {
+  constructor(eventName, date, description, image) {
+    this.eventName = eventName;
+    this.date = date;
+    this.description = description;
+    this.image = image;
+  }
 }
 
-function saveEntry(){
-   this.saveEntry.innerHTML = "";
-   for (let i= 0; i< this.entries.length; i++){
-       var entry = this.entries[i];
-       this.saveEntry.innerHTML += `<h2>${entry.name}</h2>when:${entry.date}<br>do:${entry.description}`;
-   }
-}
-// function getEntries(){
-//     var entries = [];
-//     for (var i = 0; i < entries.length; i++){
-//         $('#isOutput').append(entries[i]);
-//     }
-// }
-// $ ('#archive').click(function(){
-//     getEntries1();
-// });
+// UI class Handle UI
+class UI {
+  static displayEvents() {
+    const StoredEvents = [
+      {
+        eventName: "Party",
+        date: "14/08/2020",
+        description: "House party at Johns",
+        image: "./assets/pepper.jpg",
+      },
+      {
+        eventName: "Wedding",
+        date: "10/08/2020",
+        description: "Wedding attended for best friend",
+        image: "./assets/pepper.jpg",
+      },
+      {
+        eventName: "Interview",
+        date: "01/08/2020",
+        description: "Went for an interview that i failed",
+        image: "./assets/pepper.jpg",
+      },
+      {
+        eventName: "Interview",
+        date: "01/08/2020",
+        description: "Went for an interview that i failed",
+        image: "./assets/pepper.jpg",
+      },
+    ];
 
-// $('#add').on('click', function(){
-//     $('input').each (function(){
-//         var id = $(this).attr ('id');
-//         var value = $(this).val();
-//         localStorage.setItem(id,value);
-//     });
-// });
-// $('#load').on('click', function(){
-//     $('input').each(function(){
-//         var id = $(this).attr('id');
-//         var value = localStorage.getItem(id);
-//         $(this).val(value);
-//     });
+    const events = StoredEvents;
+
+    events.forEach((event) => UI.addEventToList(event));
+  }
+
+  static addEventToList(event) {
+    const list = document.querySelector("#book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+            <td>${event.eventName}</td>
+            <td>${event.date}</td>
+            <td>${event.description}</td>
+            <td><a href="#" class="fas fa-trash-alt text-danger delete"></a></td>
+        `;
+
+      list.appendChild(row);
+  }
+
+  static clearFields() {
+    document.querySelector("#date").value = "";
+    document.querySelector("#name").value = "";
+    document.querySelector("#file").value = "";
+    document.querySelector("#summernote").value = "";
+  }
+}
+
+// Local Storage
+
+// Display Events
+document.addEventListener('DOMContentLoaded', UI.displayEvents);
+
+
+// Add event
+document.querySelector('#form-control').addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // Get form values:
+  const eventTitle = document.querySelector('#name').value;
+  const dateTime = document.querySelector('#date').value;
+  // const imageUpload = document.querySelector('#imageUpload').value;
+  const textArea = document.querySelector('#summernote').value;
+  
+  // instiate event
+  const event = new Event(eventTitle, dateTime, textArea);
+  console.log(event);
+  
+  // Add event to UI
+  UI.addEventToList(event);
+  
+  // Clear fields
+  UI.clearFields();
+});
+
+
+// Remove an Event
+// document.querySelector('.event-list').addEventListener('click', (e) => {
+//     console.log(e.target);
 // });
